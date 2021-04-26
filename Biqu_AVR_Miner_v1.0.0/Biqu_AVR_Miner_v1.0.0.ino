591e2de3a701d40fc8f969095b4c57a9ad72c331
BiquCraft <82400252+BiquCraft@users.noreply.github.com>
2021-04-26 02:53:59 +0200

    Add files via upload

--- Biqu_AVR_Miner_v1.0.0/Biqu_AVR_Miner_v1.0.0.ino
+++ Biqu_AVR_Miner_v1.0.0/Biqu_AVR_Miner_v1.0.0.ino
@@ -1,31 +1,38 @@
 #include<stdio.h>
 #include <math.h>
-#include "hash.c"
+
 
 void setup() {
   //otwarcie portu i ustawienie prędkości na 9600 bodów
   Serial.begin(115200);
-  while(!Serial);
+  randomSeed(analogRead(0));
+  while (!Serial);
   Serial.println("Witaj milego koapnia!");
   delay (5100);
 }
- 
-int a = 0;
- 
-void loop(void) {
 
-  delay (300);
+
+void loop() {
+
+  delay (10);
   int a = 0;
   double MM = 0.0000000001;
   int job = 0;
+
+
+
+  
   do
   {
-    Serial.println("Biqu-Coin * 1,35s * 100H/s * Job Accepted : " + String(job) + " * "+ String(MM,DEC) + " Mined of BQU" );
-    
+    Serial.println("Block Hash: " + random(10000000000));
+    Serial.println("Biqu-Coin * 1,35s * 100H/s * Job Accepted : " + String(job) + "/" + String(job) + " * " + String(MM, DEC) + " Mined of BQU" );
+
     delay (300);
     MM = MM + 0.0000006556;
     job++;
     delay (1000);
+
   } while (a == 0);
+
   return;
 }
