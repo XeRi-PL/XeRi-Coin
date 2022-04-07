9c7efc89eef59c9549b427f2872fe995859ac6bf
XeRi_PL <82400252+XeRi-PL@users.noreply.github.com>
2022-04-07 22:41:58 +0200

    Update Console Wallet v0.1.4.py

--- Console Wallet v0.1.4.py
+++ Console Wallet v0.1.4.py
@@ -1,4 +1,7 @@
-from ast import Continue
+from argparse import ONE_OR_MORE
+from ast import Continue, Pass
+from asyncio.windows_events import NULL
+from fileinput import close
 from multiprocessing.connection import wait
 import os
 from cgitb import text
@@ -6,6 +9,8 @@ from re import I
 import socket
 import sys
 import time
+from os import _exit, execl, mkdir
+from turtle import back
 import colorama
 import threading
 from datetime import datetime
@@ -18,35 +23,98 @@ from time import sleep
 from threading import Lock
 colorama.init()
 i = int(0)
-
+j = int(0)
+v = int(0)
 ###########################################################
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1',1233))
 ###########################################################
-
-def start():
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+def start(i,j):
+    global sender, recipter , how_many , gpu2
     try:
         while True:
-  
-            sender = input('Write your name : ')
-            recipter = input('Write friend username: ')
-            sleep(1)
-            how_many = input('How much send XeriCoin to friend ? : ')
-            sleep(1)
-            gpu2 = input("GOOD ? :")
-            hh= 0.0
-            if gpu2 == "y":
+            asd = (""+ ","+ "0.0" +","+ str(sender) + "," + str(recipter) + "," + str(float(how_many))+","+""+","+""+","+"TT")
+            client.send(asd.encode('utf-8'))
+            msg = client.recv(2024).decode('utf-8')
+            msg = msg.split(",")
+            if msg[7] == "TT":
                 sleep(1)
-                asd =("TT"+","+ "0.0" +","+ str(sender) + "," + str(recipter) + "," + str(how_many))
-                client.send(asd.encode('utf-8'))
-                sleep(5)
+                print("----------------------------------------Transaction success !!!---------------------------------------")
+                print("Transacion hash : " + Fore.YELLOW + str(msg[6]) + Fore.RESET + " You Send : " + 
+                Fore.LIGHTCYAN_EX + str(float(msg[5])) + Fore.RESET + " to : " + Fore.LIGHTMAGENTA_EX + str(msg[4]) + Fore.RESET)
+                while True:
+                    asdd = (str(msg[6])+ ","+ "0.0" +","+ str(sender) + "," + str(recipter) + "," + str(float(how_many))+","+""+","+""+","+"TTT")
+                    client.send(asdd.encode('utf-8'))
+                    sleep(0.1)
+            if msg[0]== "GOOD":
+                #sleep()
+                gpu2 = "n"
+                sleep(0.2)
+                
+                
+                #client.close()
+                #print("")
+                #print("This window closed in 30s ..")
+                #sys.argv.clear
+                #main(sys.argv)
+                
+                #sleep(30)
+                #_exit(0)
 
-            print("xxx")
-            Fore.RESET
-
+            else:
+                sleep(1)
+                Fore.RESET
+                
     except Exception as e:
-            print(Fore.LIGHTRED_EX +'Error STATS : ' + str(e))
+        NULL
+        #print(Fore.LIGHTRED_EX +'Error STATS : ' + str(e))
 
-print(Fore.GREEN + "[STARTING]")
-Fore.RESET
-start()
+def main(argv):
+
+    global sender, recipter , how_many , one , gpu2
+    print("Menu : ")
+    print("1. check your amount")
+    print("2. send amount to friend")
+    one = input("Optipon (1/2) :")
+    if one == "1":
+        clear()
+        print("amount")
+    elif one == "2":
+        clear()
+        sender = input('Write your name : ')
+        recipter = input('Write friend username : ')
+        how_many = input('How much send XeriCoin to friend ? : ')
+        try:
+            Fore.RESET
+            print(' -- Transaction :' + Fore.YELLOW + f'{how_many}' +Fore.RESET + ' from : ' + Fore.LIGHTCYAN_EX 
+            + f'{sender}' + Fore.RESET + ' to : ' + Fore.LIGHTMAGENTA_EX + f'{recipter}' + Fore.RESET +  '. -- ')
+            Fore.RESET
+            gpu2 = input(Fore.RESET + "   ---------- Confirm ? (y/n): ---------- ")
+            while True:
+                
+                Fore.RESET
+                if gpu2 == "y":
+                    Fore.RESET
+            #start(i,j)
+                    hread2 = threading.Thread(target=start, args=(i, j))
+                    hread2.start()
+                    print(Fore.GREEN + "... ... waiting to procces transaction ... ..." + Fore.RESET)
+                    sleep(5)
+                    
+                if gpu2 == "n":
+                    print("")
+                    sys.argv.clear
+                    main(sys.argv)
+                    
+
+        except Exception as e:
+            NULL
+        #print(Fore.LIGHTRED_EX +'Error STATS : ' + str(e))
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except Exception as e:
+        print("")
+    input()
