d5676d8b74cc90108c7b7405a3d95c5b7509a41a
XeRi_PL <82400252+XeRi-PL@users.noreply.github.com>
2022-06-29 20:57:53 +0200

    Update AVR_Miner_v0.1.4.py

--- AVR_Miner_v0.1.4.py
+++ AVR_Miner_v0.1.4.py
@@ -9,22 +9,18 @@
 import time
 from time import sleep
 from time import ctime, sleep, strptime, time
-
 from serial import Serial
 import serial.tools.list_ports
 from serial import win32
 import serial, time, sys, threading
 from serial.serialutil import SerialBase, SerialException, to_bytes
-
 import json
 from json import load as jsonload
 from locale import LC_ALL, getdefaultlocale, getlocale, setlocale
-
 from colorama import Fore, Back, Style
 from termcolor import colored
 from colorama import init
 init(autoreset=True)
-
 import socket
 from pathlib import Path
 import os
@@ -33,16 +29,12 @@ from _thread import *
 from threading import Thread
 from threading import Thread as thrThread
 from threading import Lock as thread_lock
-
 from threading import Semaphore
 printlock = Semaphore(value=1)
-
 from re import sub
 import subprocess
-
 import configparser
 from configparser import ConfigParser
-
 from os import _exit, execl, mkdir
 from os import name as osname
 from os import system as ossystem
@@ -62,33 +54,13 @@ avrport = ''
 RESOURCES_DIR = "AVRMiner_" + str(MINER_VER) + "_resources"
 config = ConfigParser()
 ping_mean = []
-host = 'xeri.sytes.net'
+host = '127.0.0.1'
 portx = '1233'
 x=''
-#if not path.exists(RESOURCES_DIR):  mkdir(RESOURCES_DIR)
 
 # Create resources folder if it doesn't exist
 if not path.exists(RESOURCES_DIR):
     mkdir(RESOURCES_DIR)
-
-config = configparser.ConfigParser()
-
-config.add_section('config')
-
-config['config']['host'] = '127.0.0.1'
-config['config']['user'] = ''
-config['config']['passwd'] = ''
-config['config']['portx'] = '1233'
-
-with open('AVRMiner_0.1.3_resources/avr.ini', 'w') as f:
-    config.write(f)
-    
-config = configparser.ConfigParser()
-config.read('AVRMiner_0.1.3_resources/avr.ini')
-host = config['config']['host']
-user = config['config']['user']
-passwd = config['config']['passwd']
-portx = config['config']['portx']
 
 def now():
     # Return datetime object
@@ -105,8 +77,6 @@ def get_string(string_name):
         return lang_file["english"][string_name]
     else:
         return "String not found: " + string_name
-
-#payload = 'Client 1'
 
 def greeting():
     # greeting message depending on time
@@ -126,11 +96,6 @@ def greeting():
     else:
         greeting = print('good_back') 
 
-#print('Miner configuration:')
-#print(f'config: {host}')
-#print(f'config: {user}')
-#print(f'config: {passwd}')
-#print(f'config: {portx}')
 if system() == 'Darwin':
     if getlocale()[0] is None:
         setlocale(LC_ALL, 'en_US.UTF-8')
@@ -150,7 +115,7 @@ client.connect((host,1233))
 
 print(colored(Style.BRIGHT + 'Waiting for device' , 'white' , 'on_blue'));
 print(colored(Style.BRIGHT + '##########################################' , 'white' , 'on_cyan'));
-print(colored(Style.BRIGHT + '# XeRi-Coin Python AVRMiner(v0.1.4)      #' , 'white' , 'on_cyan'));
+print(colored(Style.BRIGHT + f'# XeRi-Coin Python AVRMiner(v{MINER_VER})      #' , 'white' , 'on_cyan'));
 print(colored(Style.BRIGHT + '# https://github.com/XeRi-PL/XeRi-Coin   #' , 'white' , 'on_cyan'));
 print(colored(Style.BRIGHT + '# Distributed under MIT license          #' , 'white' , 'on_cyan'));
 print(colored(Style.BRIGHT + '# © XeRi-Coin Community 2021 - 2022      #' , 'white' , 'on_cyan'));
@@ -185,8 +150,7 @@ if not Path(str(RESOURCES_DIR) + '/Miner_config.cfg').is_file():
         print(str(p))
         port_names =[]
         for p in ports:
-            
-                #port_names.append(ports.device)
+
             avrport = ''
     
     while True:
@@ -197,13 +161,11 @@ if not Path(str(RESOURCES_DIR) + '/Miner_config.cfg').is_file():
         else:
             break
 
-            #config['avr'] = {'avrport': avrport }
-
     config = configparser.ConfigParser()
     config.add_section('avr')
     config['avr']['avrport'] = avrport
         
-        # Write data to file
+    # Write data to file
     with open('AVRMiner_0.1.4_resources/Miner_config.cfg', 'w') as d:
         config.write(d)
 else:
@@ -213,55 +175,105 @@ else:
     avrport = avrport.replace(" ","").split(',')
 print("SAVED !")
 
+def port_num(avrport):
+    return str(''.join(filter(str.isdigit, avrport)))
 
-def miner(avrport):        
+
+def miner(avrport,):
+    global xxd , i
+    job_good = 0
+    job_all = 0
 
     sleep(1)
     try:
         print("Mining started <3 ")
         sleep(1)
-        
         while True:
-            sleep(1)
+            sleep(2)
             i = 0
-            for i in range(100000):
+            for i in range(10_000):
             
-                ser = Serial(avrport,baudrate = 115200,timeout = 1)
-            
-                
+                xxd = Serial(avrport,baudrate = 115200,timeout = 2.2)
                 i += 1
-    except:
-        
+    except:  
         sleep(1)
     try:
         
         while True:
+            responsetimetart = time.time()
+            Speedhash = 0.000260 * i 
+            list_gpus = "AVR_Nano"
 
-            #val = ser.read_until(b'\n').decode().strip().split(',')
-            val = ser.readline().decode().strip('off \r')
-            valA = val.split('\n')
-            client.send(val.encode('utf-8'))
+            config = configparser.ConfigParser()
+            config.read('AVRMiner_0.1.4_resources/avr.ini')
+            user = config['config']['user']
             
-        #client_socket.send(user.encode('utf-8'))
-            print( colored(now().strftime(Style.BRIGHT + "%H:%M:%S") , 'white' , 'on_yellow' ) + val ,flush=True)
+            xdc = (str(user)+ "," + str(round(float(Speedhash),7)) +","+""+","+""+","+""+","+ str(list_gpus)+","+""+","+"AVR")
+            client.send(xdc.encode('utf-8'))
+            
+            uwu = client.recv(400).decode('utf-8')
+            uwu = uwu.split(',')
+            
+            if uwu[0] == "11":
+                xxd.write(uwu[2].encode('utf-8'))
+            
+            val = xxd.readline().decode().strip() #'off \r'
+            valA = val.split(',') #'\n'
+            sleep(0.2)
+            if val == "08e1000401d500000000000008e1000405cf08e1" or val == "08e1000001d500000000000008e1000005cf08e1":
+                job_good = job_good + 1
+            else:
+                job_good = job_good - 1
+            job_all = job_all + 1
+            
+            responsetimestop = time.time()
+            ping = ((responsetimestop - responsetimetart) + 0.1) * 10
+
+            print(Fore.LIGHTBLUE_EX + "COM" + port_num(avrport) + " - " 
+            + now().strftime(Style.BRIGHT + "%H:%M:%S")
+            + " - " 
+            + Fore.YELLOW + "Hashrate: " + str(Speedhash) + " MH/s ( " + str(Speedhash * 1000000 ) + " kH/s )"
+            + Fore.LIGHTCYAN_EX + " - Share's: " + str(job_good) + "/" + str(job_all) 
+            + Fore.GREEN + " Ping: " + str(round(float(ping),2)) + " ms")
+
+            sleep(1.6)
         else:
             raise Exception("No data received from AVR")
         sleep(1)    
     except Exception as e:
         print('Error launching AVR Miner: ' + str(e))
         
-    
-
 #-----------------------------------------------------------------------------------------------------
-#if __name__ == '__main__':
-    
+
 try:
-    for i in range(len(avrport)):
-        y = i
-        y = threading.Thread(target=miner, args =(avrport[i],))
+    if not Path(str(RESOURCES_DIR) + '/avr.ini').is_file():
+
+        config = configparser.ConfigParser()
+        config.add_section('config')
+
+        config['config']['user'] = ''
+
+        with open('AVRMiner_0.1.4_resources/avr.ini', 'w') as f:
+            config.write(f)
+        sleep(5)
+        print("!!!! Uzupełnij user w arv.ini !!!!")
+        sleep(5)
+    else:
+
+        config = configparser.ConfigParser()
+        config.read('AVRMiner_0.1.4_resources/avr.ini')
+        user = config['config']['user']
+        user_input = input("Podaj swój nick : \n")
+
+        if user_input == user:
+
+            for i in range(len(avrport)):
+                y = i
+                y = threading.Thread(target=miner, args =(avrport[i],))
         #y.daemon = True
         #sleep(1)
         #threading.append(i)
-        y.start()
+                y.start()
 except:
-    print("Error: unable to start thread")
+        print("Error: unable to start thread")
+
